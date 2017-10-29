6d37cfdab6b1cd25c502bdb0ec5440af12315512
Carl Cortright <ckcortright@gmail.com>
2017-10-28 18:29:11 -0600

    Upload working. Fixed some caching issues

--- server.py
+++ server.py
@@ -1,11 +1,51 @@
-from flask import Flask, render_template, send_from_directory
-app = Flask(__name__, static_url_path='/static/')
+import os
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for, flash
+from werkzeug.utils import secure_filename
+import random
 
+################################################################################
+# App config
+################################################################################
+UPLOAD_FOLDER = 'data/'
+ALLOWED_EXTENSIONS = set(["csv"])
+app = Flask(__name__, static_url_path='/static/')
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config['SESSION_TYPE'] = 'filesystem'
+
+################################################################################
+# GET Routes
+################################################################################
 @app.route('/')
 def root():
-    return render_template('index.html')
+    fname = request.args.get('filename')
+    if fname == None:
+        fname = "examples/example.csv"
+    return render_template('index.html', LocationData=fname)
 
 @app.route('/data/<path:filename>', methods=["GET"])
 def getData(filename):
-    print(filename)
     return send_from_directory("data/", filename)
+
+################################################################################
+# File upload
+################################################################################
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/data', methods=["POST"])
+def uploadData():
+    # check if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return redirect(url_for('root', filename=filename ))
--- static/js/map.js
+++ static/js/map.js
@@ -4,7 +4,7 @@
 * Author: Carl Cortright
 */
 
-CSV_DATA = []
+var CSV_DATA = []
 
 /*
 * Creates a tooltip for each
@@ -16,85 +16,92 @@ function tooltipHtml(n, d){	/* function to create html content string in tooltip
 }
 
 /*
-* Loads the data from CSV
+* Gets the url parameters
 */
-function loadData(path){
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
 
-}
+  for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+          return sParameterName[1] === undefined ? true : sParameterName[1];
+      }
+  }
+};
 
 /*
 * Updates the map when a fund is selected/deselected
 */
-function updateMap(color) {
-  d3.csv("data/example.csv", function(data) {
-    var StatesData = []
-    var statesData = ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
-    "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
-    "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
-    "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
-    "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
-    var totalNum = 0;
-    var largestState = 0;
-    statesData.forEach(function(d){
-        var num=0;
-        var inv=0;
-        for(i = 0; i < data.length; i++){
-          var obj = data[i]
-          if (obj.State === d) {
-            num += 1;
-          }
+function updateMap(color, data) {
+  var StatesData = []
+  var statesData = ["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+  "ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH",
+  "MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT",
+  "CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN",
+  "WI", "MO", "AR", "OK", "KS", "LS", "VA"]
+  var totalNum = 0;
+  var largestState = 0;
+  statesData.forEach(function(d){
+      var num=0;
+      var inv=0;
+      for(i = 0; i < data.length; i++){
+        var obj = data[i]
+        if (obj.State === d) {
+          num += 1;
         }
-        if(num>largestState){
-          largestState = num;
-        }
-        totalNum += num
-      });
-
-    statesData.forEach(function(d){
-        var num=0;
-        var inv=0;
-        for(i = 0; i < data.length; i++){
-          var obj = data[i]
-          if (obj.State === d) {
-            num += 1;
-          }
-        }
-        StatesData[d]={num:num, inv:inv.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), color:d3.interpolate("#FFFFFF", color)(num/largestState)};
-      });
-
-    var width = 1000;
-    var height = 600;
-
-    // D3 Projection
-    var projection = d3.geo.albersUsa()
-    				   .translate([(width/2) - 27.5, (height/2) - 17])  // translate to center of screen
-    				   .scale([1265]);
-
-    /* draw states on id #statesvg */
-    d3.select("svg").selectAll("*").remove()
-
-    uStates.draw("#statesvg", StatesData, tooltipHtml);
-    let elements = Array.from(document.getElementsByClassName("state"));
-    elements.forEach(function(elem){
-      elem.style.stroke = color;
+      }
+      if(num>largestState){
+        largestState = num;
+      }
+      totalNum += num
     });
 
-    for(i=0; i < data.length; i++){
-      var d = data[i]
-      if(true){
-        d3.select("#statesvg")
-      	.append("circle")
-      	.attr("cx", projection([d.Longitude, d.Latitude])[0])
-      	.attr("cy", projection([d.Longitude, d.Latitude])[1])
-      	.attr("r", 5)
-      	.style("fill", "#1E2832")
-      	.style("opacity", 0.6);
+  statesData.forEach(function(d){
+      var num=0;
+      var inv=0;
+      for(i = 0; i < data.length; i++){
+        var obj = data[i]
+        if (obj.State === d) {
+          num += 1;
+        }
       }
-    }
+      StatesData[d]={num:num, inv:inv.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","), color:d3.interpolate("#FFFFFF", color)(num/largestState)};
+    });
 
+  var width = 1000;
+  var height = 600;
+
+  // D3 Projection
+  var projection = d3.geo.albersUsa()
+              .translate([(width/2) - 27.5, (height/2) - 17])  // translate to center of screen
+              .scale([1265]);
+
+  /* draw states on id #statesvg */
+  d3.select("svg").selectAll("*").remove()
+
+  uStates.draw("#statesvg", StatesData, tooltipHtml);
+  let elements = Array.from(document.getElementsByClassName("state"));
+  elements.forEach(function(elem){
+    elem.style.stroke = color;
   });
 
+  for(i=0; i < data.length; i++){
+    var d = data[i]
+    if(true){
+      d3.select("#statesvg")
+      .append("circle")
+      .attr("cx", projection([d.Longitude, d.Latitude])[0])
+      .attr("cy", projection([d.Longitude, d.Latitude])[1])
+      .attr("r", 5)
+      .style("fill", "#1E2832")
+      .style("opacity", 0.6);
+    }
+  }
 }
 
-updateMap("#EA862D");
+d3.csv("data/" + dataFile + "?q=" + Math.floor(Math.random() * 10000000), function(data) { CSV_DATA = data; updateMap("#EA862D", CSV_DATA); });
 d3.select(self.frameElement).style("height", "600px");
